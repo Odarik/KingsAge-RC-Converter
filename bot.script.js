e2c197c1bee0bcab0c52260c7e5acef2e8324881
Odarik <axel.ogcn@hotmail.fr>
2015-06-21 21:51:04 +0200

    Create bot.script.js

---
+++ bot.script.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        Robot RC
+// @namespace   Robot RC
+// @include     http://board.fr.kingsage.gameforge.com/*
+// @version     1
+// @grant		   GM_getValue
+// @grant		   GM_setValue
+// ==/UserScript==
+
+
+var RC_saved = "Test bot pour script24"
+
+if(/board.fr.kingsage.gameforge.com/.test(location.href))
+  {
+var bouton = document.createElement('input'); //Affichage du bouton ajouter à côté de transmettre/supprimer
+  bouton.innerHTML = 'Envoyer'
+  bouton.setAttribute('type', 'reset');
+  bouton.setAttribute('id', 'ajouter');
+  bouton.setAttribute('value', 'Robot');
+  bouton.setAttribute('name', 'Robot');
+  bouton.setAttribute('tabindex', '8');
+  document.querySelector('.formSubmit').appendChild(bouton);
+
+
+
+ //Fonction qui efface la fenêtre
+  document.getElementById('ajouter').addEventListener('click', function (event)
+  {     
+    window.open('http://board.fr.kingsage.gameforge.com/board30-les-mondes/board293-monde-20/board297-taverne-des-chevaliers/13851-la-taverne-de-la-c%C3%A9r%C3%A9ale-enchant%C3%A9e/?bouton=script');  
+    
+  }, true);
+  }
+if(/bouton=script/.test(location.href))
+  {
+    var url2 = document.getElementById('replyButton1').href;
+    url2 +='&bouton=url2';
+    window.location.replace(url2);
+    //window.open(url2);
+   
+  }
+
+if(/bouton=url2/.test(location.href))
+      {
+        var RC_saved = GM_getValue('rc', '');
+        document.getElementById('text').innerHTML = RC_saved;
+        document.getElementsByName('send')[1].click();
+      }
+
+
+        
