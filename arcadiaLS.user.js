c73ff2c3add9f6af3b5e5364104239a7f4e35def
Odarik <axel.ogcn@hotmail.fr>
2015-12-13 16:53:22 +0100

    Update arcadiaLS.user.js

--- arcadiaLS.user.js
+++ arcadiaLS.user.js
@@ -2,41 +2,66 @@
 // @name        LS Arcadia
 // @namespace   LS Arcadia
 // @author      Odarik
-// @version     1.0.2
+// @version     1.0.5
 // @include     http://www.arcadia-forum.com/*
-// @grant       none
+// @updateURL   https://github.com/Odarik/KingsAge-RC-Converter/raw/master/arcadiaLS.user.js
+// @downloadURL https://github.com/Odarik/KingsAge-RC-Converter/raw/master/arcadiaLS.user.js
+// @grant		   GM_getValue
+// @grant		   GM_setValue
 // ==/UserScript==
+// == Compatibilité navigateur ==
+var Chrome = navigator.userAgent.indexOf('Chrome') > - 1;
+if (Chrome)
+{
+  this.GM_getValue = function (key, def) {
+    return localStorage[key] || def;
+  };
+  this.GM_setValue = function (key, value) {
+    return localStorage[key] = value;
+  };
+  this.GM_deleteValue = function (key) {
+    return delete localStorage[key];
+  };
+}
+// == Scripts ==
+
 if (/libre-service/.test(location.href))
 {
   var bouton = document.createElement('a');
   bouton.innerHTML = 'Script LS Graph'
   bouton.setAttribute('id', 'scriptLSGraph');
-  bouton.setAttribute('style', 'cursor:pointer;position:absolute;left:140px;');
+  bouton.setAttribute('style', 'cursor:pointer;position:absolute;left:120px;');
   document.querySelectorAll('.ls_nav') [0].appendChild(bouton);
   var bouton = document.createElement('a');
   bouton.innerHTML = 'Script LS Codage'
   bouton.setAttribute('id', 'scriptLSCod');
-  bouton.setAttribute('style', 'cursor:pointer;position:absolute;left:250px;');
+  bouton.setAttribute('style', 'cursor:pointer;position:absolute;left:220px;');
+  document.querySelectorAll('.ls_nav') [0].appendChild(bouton);
+  var bouton = document.createElement('a');
+  bouton.innerHTML = 'Ajouter au LS +'
+  bouton.setAttribute('id', 'scriptLSplus');
+  bouton.setAttribute('style', 'cursor:pointer;position:absolute;left:330px;');
   document.querySelectorAll('.ls_nav') [0].appendChild(bouton);
   var contenu = '';
-  var qeel = '';
-  var header = '';
-  var pa = '';
-  var categorie = '';
-  var message = '';
-  var autres = '';
+  var qeel = GM_getValue('qeel', '');
+  var header = GM_getValue('header', '');
+  var pa = GM_getValue('pa', '');
+  var categorie = GM_getValue('categorie', '');
+  var message = GM_getValue('message', '');
+  var autres = GM_getValue('autres', '');
   var contenufin = '';
-  var headerban = '';
-  var avatar = '';
-  var signature = '';
-  var autresgraph = '';
-  var fiche ='';
+  var headerban = GM_getValue('headerban', '');
+  var avatar = GM_getValue('avatar', '');
+  var signature = GM_getValue('signature', '');
+  var autresgraph = GM_getValue('autresgraph', '');
+  var fichepub = GM_getValue('fichepub', '');
+  var ficherp = GM_getValue('ficherp', '');
   var newElement = document.createElement('div');
-  newElement.innerHTML = '<div id="closeLS" style="cursor:pointer;position:relative;top:200px;text-decoration: none;left:25%;  font-size: 35px;  font-weight: bold;  color: #fff;margin-bottom:10px;">×</div><center><textarea style="width:50%;height:700px;margin-top:200px;" id="textareaLS"></textarea></center>';
-  newElement.setAttribute('style', 'width:100%;height:100%;background-color: rgba(0, 0, 0, 0.4);position:absolute;top:0px;z-index:600;display:none;');
+  newElement.innerHTML = '<div id="closeLS" style="cursor:pointer;position:relative;top:150px;text-decoration: none;left:25%;  font-size: 35px;  font-weight: bold;  color: #fff;margin-bottom:10px;">×</div><center><textarea style="width:50%;height:700px;margin-top:150px;" id="textareaLS"></textarea></center>';
+  newElement.setAttribute('style', 'width:100%;height:100%;background-color: rgba(0, 0, 0, 0.4);position:fixed;top:0px;z-index:600;display:none;');
   newElement.setAttribute('id', 'divLS');
   document.body.appendChild(newElement);
-  document.getElementById('scriptLSCod').addEventListener('click', function (event)
+  document.getElementById('scriptLSplus').addEventListener('click', function (event)
   {
     for (i = 0; i < document.querySelectorAll('.ls_infos.ls_red').length; i++) {
       contenu = document.querySelectorAll('.ls_infos.ls_red') [i].innerHTML.replace(/.*(<a class="ls_topictitle.*".*>;*<\/a>).*/g, '$1').replace(/<img [^>]*>/, '').replace(/(<a[^<]*<\/a>).*/, '$1').replace(/class="ls_topictitle ls_red"/, '') + '\n';
@@ -55,29 +80,16 @@ if (/libre-service/.test(location.href))
       if (contenu.search(/\[header\]/i) != - 1) {
         header += '[*]' + contenu;
       }
-      if (contenu.search(/\[fiche.*\]/i) != - 1) {
-        fiche += '[*]' + contenu;
+      if (contenu.search(/\[fiche pub\]/i) != - 1) {
+        fichepub += '[*]' + contenu;
       }
-      if ((contenu.search(/\[qeel\]/i) == - 1) && (contenu.search(/\[pa\]/i) == - 1) && (contenu.search(/\[profil\]/i) == - 1) && (contenu.search(/\[catégories\]/i) == - 1) && (contenu.search(/\[header\]/i) == - 1) && (contenu.search(/\[fiche.*\]/i) != - 1))
+      if (contenu.search(/\[fiche rp\]/i) != - 1) {
+        ficherp += '[*]' + contenu;
+      }
+      if ((contenu.search(/\[qeel\]/i) == - 1) && (contenu.search(/\[pa\]/i) == - 1) && (contenu.search(/\[profil\]/i) == - 1) && (contenu.search(/\[catégories\]/i) == - 1) && (contenu.search(/\[header\]/i) == - 1) && (contenu.search(/\[fiche rp\]/i) == - 1) && (contenu.search(/\[fiche pub\]/i) == - 1))
       {
         autres += '[*]' + contenu;
       }
-    }
-    contenufin = '<div class="titre_ls">Récapitulatif du Libre Service</div>\n\n[center]Ce topic reprend tous les liens des libres services présents dans ce sous-forum. Vous n&apos;avez plus qu&apos;à faire votre choix ![/center]\n\n' +
-    '<div class="soustitre_ls" >• Header et Footer</div>\n[list]' + header + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Page d&apos;Acceuil</div>\n[list]' + pa + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Catégories et Forums</div>\n[list]' + categorie + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• QEEL</div>\n[list]' + qeel + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Messages et Profils</div>\n[list]' + message + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Fiche Pub et RP</div>\n[list]' + fiche + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Autres</div>\n[list]' + autres + '[/list]';
-    document.getElementById('textareaLS').innerHTML = contenufin;
-    document.getElementById('divLS').style.display = 'table';
-  }, true);
-  document.getElementById('scriptLSGraph').addEventListener('click', function (event)
-  {
-    for (i = 0; i < document.querySelectorAll('.ls_infos.ls_red').length; i++) {
-      contenu = document.querySelectorAll('.ls_infos.ls_red') [i].innerHTML.replace(/.*(<a class="ls_topictitle.*".*>;*<\/a>).*/g, '$1').replace(/<img [^>]*>/, '').replace(/(<a[^<]*<\/a>).*/, '$1').replace(/class="ls_topictitle ls_red"/, '') + '\n';
       if ((contenu.search(/header/i) != - 1) || (contenu.search(/bannière/i) != - 1)) {
         headerban += '[*]' + contenu;
       }
@@ -92,16 +104,57 @@ if (/libre-service/.test(location.href))
         autresgraph += '[*]' + contenu;
       }
     }
+    GM_setValue('qeel', qeel);
+    GM_setValue('header', header);
+    GM_setValue('pa', pa);
+    GM_setValue('categorie', categorie);
+    GM_setValue('message', message);
+    GM_setValue('autres', autres);
+    GM_setValue('headerban', headerban);
+    GM_setValue('avatar', avatar);
+    GM_setValue('signature', signature);
+    GM_setValue('autresgraph', autresgraph);
+    GM_setValue('fichepub', fichepub);
+    GM_setValue('ficherp', ficherp);
+  }, true);
+  document.getElementById('scriptLSCod').addEventListener('click', function (event)
+  {
     contenufin = '<div class="titre_ls">Récapitulatif du Libre Service</div>\n\n[center]Ce topic reprend tous les liens des libres services présents dans ce sous-forum. Vous n&apos;avez plus qu&apos;à faire votre choix ![/center]\n\n' +
-    '<div class="soustitre_ls" >• Header et Bannières</div>\n[list]' + headerban + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Avatarl</div>\n[list]' + avatar + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Signatures</div>\n[list]' + signature + '[/list]\n\n' +
-    '<div class="soustitre_ls" >• Autres</div>\n[list]' + autresgraph + '[/list]';
+    '<div class="soustitre_ls" >• Header et Footer</div>\n[list]' + GM_getValue('header', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Page d&apos;Acceuil</div>\n[list]' + GM_getValue('pa', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Catégories et Forums</div>\n[list]' + GM_getValue('categorie', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• QEEL</div>\n[list]' + GM_getValue('qeel', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Messages et Profils</div>\n[list]' + GM_getValue('message', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Fiches Pub</div>\n[list]' + GM_getValue('fichepub', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Fiches RP</div>\n[list]' + GM_getValue('ficherp', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Autres</div>\n[list]' + GM_getValue('autres', '') + '[/list]';
+    document.getElementById('textareaLS').innerHTML = contenufin;
+    document.getElementById('divLS').style.display = 'table';
+  }, true);
+  document.getElementById('scriptLSGraph').addEventListener('click', function (event)
+  {
+    contenufin = '<div class="titre_ls">Récapitulatif du Libre Service</div>\n\n[center]Ce topic reprend tous les liens des libres services présents dans ce sous-forum. Vous n&apos;avez plus qu&apos;à faire votre choix ![/center]\n\n' +
+    '<div class="soustitre_ls" >• Header et Bannières</div>\n[list]' + GM_getValue('headerban', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Avatarl</div>\n[list]' + GM_getValue('avatar', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Signatures</div>\n[list]' + GM_getValue('signature', '') + '[/list]\n\n' +
+    '<div class="soustitre_ls" >• Autres</div>\n[list]' + GM_getValue('autresgraph', '') + '[/list]';
     document.getElementById('textareaLS').innerHTML = contenufin;
     document.getElementById('divLS').style.display = 'table';
   }, true);
   document.getElementById('closeLS').addEventListener('click', function (event)
   {
+    GM_setValue('qeel', '');
+    GM_setValue('header', '');
+    GM_setValue('pa', '');
+    GM_setValue('categorie', '');
+    GM_setValue('message', '');
+    GM_setValue('autres', '');
+    GM_setValue('headerban', '');
+    GM_setValue('avatar', '');
+    GM_setValue('signature', '');
+    GM_setValue('autresgraph', '');
+    GM_setValue('fichepub', '');
+    GM_setValue('ficherp', '');
     document.getElementById('divLS').style.display = 'none';
   }, true);
 }
